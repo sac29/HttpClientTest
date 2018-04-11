61261a53d30d57c4b2cc309baaf035c72a67f1ae
sachin.vishwakarma <sachin.vishwakarma@DEL1-DHP-32427>
2018-04-11 10:38:43 +0530

    consumed the api using all crud operations

--- HttpClientTest/Program.cs
+++ HttpClientTest/Program.cs
@@ -18,25 +18,28 @@ namespace HttpClientTest
     }
     class Program
     {
+        static HttpClient client = new HttpClient();
         static void Main(string[] args)
 
         {
-            CallWebApiAsync().Wait();
-            
+             CallGetOnAPi(5).Wait();
+          //  CallPostOnApi().Wait();
+         //   CallDeleteOnApi(13).Wait();
+         //   CallPutOnApi(12).Wait();
         }
 
-        static async Task CallWebApiAsync()
+        static async Task CallGetOnAPi(int id)
         {
             using(var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:54240/");
+                client.BaseAddress = new Uri("http://localhost:52967/");
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                HttpResponseMessage response = await client.GetAsync("api/students/1");
+                HttpResponseMessage response = await client.GetAsync("api/students/"+id);
                 if (response.IsSuccessStatusCode)
                 {
                     Student student1 = await response.Content.ReadAsAsync<Student>();
-                    Console.WriteLine("Id :{0}\tFirstName: {1}\tlasName: {2} \tgender: {3}\tFirstName: {4}",
+                    Console.WriteLine("Id :{0}\tFirstName: {1}\tlasName: {2} \tgender: {3}\t No of sunjects: {4}",
                         student1.ID, student1.FirstName, student1.LastName, student1.Gender, student1.NoOfSubjects);
                     Console.ReadLine();
                 }
@@ -44,21 +47,67 @@ namespace HttpClientTest
                 {
                     Console.WriteLine("inernal erver error");
                 }
-                var student = new Student()
-                {
-                    ID = 12,
-                    FirstName ="rishab",
-                    LastName ="modi",
-                    Gender = "male",
-                    NoOfSubjects = "5"
-                };
-                HttpResponseMessage res = await client.PostAsJsonAsync("api/students", student);
-                if (res.IsSuccessStatusCode)
-                {
-                    Uri returnUrl = res.Headers.Location;
-                    Console.WriteLine(returnUrl);
-                    Console.ReadLine();
-                }
+           
+            }
+        }
+        static async Task CallPostOnApi()
+        {
+            client.BaseAddress = new Uri("http://localhost:52967/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+          var student = new Student()
+            {
+                ID = 13,
+                FirstName = "rishab",
+                LastName = "modi",
+                Gender = "male",
+                NoOfSubjects = "5"
+            };
+            HttpResponseMessage res = await client.PostAsJsonAsync("api/students/", student);
+            if (res.IsSuccessStatusCode)
+            {
+                Uri returnUrl = res.Headers.Location;
+                Console.WriteLine(returnUrl);
+                Console.ReadLine();
+            }
+        }
+        static async Task CallPutOnApi(int id)
+        {
+            client.BaseAddress = new Uri("http://localhost:52967/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var student = new Student()
+            {
+                ID = 12,
+                FirstName = "shalini",
+                LastName = "modi",
+                Gender = "female",
+                NoOfSubjects = "5"
+            };
+            HttpResponseMessage res = await client.PutAsJsonAsync("api/students/"+id, student);
+            if (res.IsSuccessStatusCode)
+            {
+                Uri returnUrl = res.Headers.Location;
+                Console.WriteLine(returnUrl);
+                Console.ReadLine();
+            }
+        }
+
+        static async Task CallDeleteOnApi(int id)
+        {
+            client.BaseAddress = new Uri("http://localhost:52967/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+          
+            HttpResponseMessage res = await client.DeleteAsync("api/students/"+id);
+            if (res.IsSuccessStatusCode)
+            {
+                Uri returnUrl = res.Headers.Location;
+                Console.WriteLine(returnUrl);
+                Console.ReadLine();
             }
         }
     }
